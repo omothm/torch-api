75246530ea39f4b4cec8dc16fcf9084e2f24905a
Omar Othman <omar@omothm.com>
2020-02-26 19:02:34 +0300

    :wrench: add test_input for batch testing

--- test.py
+++ test.py
@@ -3,11 +3,11 @@
 Emulation of a server sending request to the API.
 """
 
-__author__ = "Omar Othman"
+__author__ = "Omar Othman; Emre Bicer"
 
 import datetime
 import json
-import time
+import os
 
 from torchapi.api import handle
 
@@ -17,19 +17,23 @@ def main():
     """
 
     # emulate getting a base-64 image from the front end
-    with open("example_base64.txt", "r") as base64_file:
-        image_base64 = base64_file.read()
-
-    # emulate getting a request from the front end as json
-    request = {"request": "banknote", "image": image_base64}
-    request_json = json.dumps(request)
+    (dirpath, dirnames, _) = next(os.walk(os.path.join(os.path.dirname(__file__), "test_input")))
 
     # emulate server operation
-    while True:
-        print(f"{str(datetime.datetime.now())} - Sending request")
-        response = handle(request_json)
-        print(response)
-        time.sleep(3)
+    for dirname in dirnames:
+        full_path = os.path.join(dirpath, dirname)
+        (_, _, filenames) = next(os.walk(full_path))
+        for filename in filenames:
+            with open(os.path.join(full_path, filename), "r") as base64_file:
+                image_base64 = base64_file.read()
+
+            # emulate getting a request from the front end as json
+            request = {"request": "banknote", "image": image_base64}
+            request_json = json.dumps(request)
+
+            print(f"{str(datetime.datetime.now())} - Sending request")
+            response = handle(request_json)
+            print(f"Actual class {dirname} - response = {response}")
 
 
 if __name__ == "__main__":
